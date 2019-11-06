70df1e8912d807ef6826eca41c74e6f571ac54d2
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2019-11-06 19:35:32 +0100

    Add flow

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
+// @flow
 import React from "react";
 
-export const useToggle = (initial) => {
-    const [value, setValue] = React.useState(!!initial);
+export const useToggle = (initial: ?boolean): [boolean, ()=>void, ()=>void, ()=>void] => {
+    const [value: boolean, setValue] = React.useState(!!initial)
 
-    const toggle = (event) => {
+    const toggle = (event: ?SyntheticEvent<*>) => {
         setValue(!value)
         event && event.type !== 'change' && event.preventDefault()
-    };
-    const enable = (event) => {
+    }
+    const enable = (event: ?SyntheticEvent<*>) => {
         setValue(true)
         event && event.type !== 'change' && event.preventDefault()
-    };
-    const disable = (event) => {
+    }
+    const disable = (event: ?SyntheticEvent<*>) => {
         setValue(false)
         event && event.type !== 'change' && event.preventDefault()
-    };
+    }
 
-    return [value, enable, disable, toggle];
-};
+    return [value, enable, disable, toggle]
+}
